72398add5b448d29e3348c9b66203498f3f9c836
safiroxe <40152251+safiroxe@users.noreply.github.com>
2025-06-20 21:39:40 -0300

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1750466380041

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_dist_05476590._.js",
+  "static/chunks/[root-of-the-server]__5d5bf7ed._.js",
+  "static/chunks/[root-of-the-server]__dfce79ab._.css"
+],
+    source: "dynamic"
+});
